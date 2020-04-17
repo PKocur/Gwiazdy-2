50a55f98c94e0c1a51738a51cc751fd091661980
PKocur <przemyslawkocur99@gmail.com>
2020-04-17 14:08:01 +0200

    Fix try-catch block

--- src/main/java/pl/pk99/gwiazdy/data/GwiazdyDatabaseManager.java
+++ src/main/java/pl/pk99/gwiazdy/data/GwiazdyDatabaseManager.java
@@ -10,10 +10,10 @@ import java.util.List;
 
 //Klasa bezpośrednio zarządza bazą danych (umożliwia zapisywanie, usuwanie oraz wczytywanie gwiazd z bazy)
 /*
-Schemat bazy:
-Tabela GWIAZDY w relacji 1 do 1 z tabelą DEKLINACJE
-Tabela GWIAZDY w relacji 1 do 1 z tabelą REKTASCENSJE
-Tabela GWIAZDY w relacji * do 1 z tabelą GWIAZDOZBIORY
+    Schemat bazy:
+    Tabela GWIAZDY w relacji 1 do 1 z tabelą DEKLINACJE
+    Tabela GWIAZDY w relacji 1 do 1 z tabelą REKTASCENSJE
+    Tabela GWIAZDY w relacji * do 1 z tabelą GWIAZDOZBIORY
  */
 
 public abstract class GwiazdyDatabaseManager {
@@ -67,20 +67,31 @@ public abstract class GwiazdyDatabaseManager {
         Transaction transaction = null;
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
-            List <Gwiazda> gwiazdy = session.createQuery("from Gwiazda", Gwiazda.class).list();
+            List<Gwiazda> gwiazdy = session.createQuery("from Gwiazda", Gwiazda.class).list();
             transaction.commit();
             return gwiazdy;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
         }
+        return null;
     }
 
     public static List<Gwiazdozbior> wczytajGwiazdozbioryZBazy() {
         Transaction transaction = null;
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
-            List <Gwiazdozbior> gwiazdozbiory = session.createQuery("from Gwiazdozbior", Gwiazdozbior.class).list();
+            List<Gwiazdozbior> gwiazdozbiory = session.createQuery("from Gwiazdozbior", Gwiazdozbior.class).list();
             transaction.commit();
             return gwiazdozbiory;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
         }
+        return null;
     }
-
 }
